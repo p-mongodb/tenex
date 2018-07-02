1a158d71e8d30f6453b8a1bdae5a8841305f31cc
Oleg Pudeyev <oleg@bsdpower.com>
2018-07-02 19:57:41 -0400

    Repair

--- lib/fe/presenters/project_presenter.rb
+++ lib/fe/presenters/project_presenter.rb
@@ -1,45 +1,16 @@
-class PullPresenter
+class ProjectPresenter
   extend Forwardable
 
-  def initialize(pull, eg_client)
-    @pull = pull
+  def initialize(project, eg_client)
+    @project = project
     @eg_client = eg_client
   end
 
-  attr_reader :pull
+  attr_reader :project
   attr_reader :eg_client
-  def_delegators :@pull, :[]
+  def_delegators :@project, :[], :display_name
 
-  def statuses
-    @statuses ||= @pull.statuses.map do |status|
-      EvergreenStatusPresenter.new(status, @pull, eg_client)
-    end
-  end
-
-  def take_status(label)
-    status = statuses.detect { |s| s['context'] == label }
-    if status
-      @taken_statuses ||= {}
-      @taken_statuses[status.context] = true
-    end
-    status
-  end
-
-  def untaken_statuses
-    statuses.reject do |status|
-      @taken_statuses && @taken_statuses[status['context']]
-    end
-  end
-
-  def top_evergreen_status
-    status = @pull.top_evergreen_status
-    if status
-      status = EvergreenStatusPresenter.new(status, @pull, eg_client)
-    end
-    status
-  end
-
-  def evergreen_version
-    @evergreen_version ||= Evergreen::Version.new(eg_client, @pull.evergreen_version_id)
+  def identifier
+    @project['identifier']
   end
 end
--- lib/fe/presenters/pull_presenter.rb
+++ lib/fe/presenters/pull_presenter.rb
@@ -1,16 +1,45 @@
-class ProjectPresenter
+class PullPresenter
   extend Forwardable
 
-  def initialize(project, eg_client)
-    @project = project
+  def initialize(pull, eg_client)
+    @pull = pull
     @eg_client = eg_client
   end
 
-  attr_reader :project
+  attr_reader :pull
   attr_reader :eg_client
-  def_delegators :@project, :[], :display_name
+  def_delegators :@pull, :[], :repo_full_name
 
-  def identifier
-    @project['identifier']
+  def statuses
+    @statuses ||= @pull.statuses.map do |status|
+      EvergreenStatusPresenter.new(status, @pull, eg_client)
+    end
+  end
+
+  def take_status(label)
+    status = statuses.detect { |s| s['context'] == label }
+    if status
+      @taken_statuses ||= {}
+      @taken_statuses[status.context] = true
+    end
+    status
+  end
+
+  def untaken_statuses
+    statuses.reject do |status|
+      @taken_statuses && @taken_statuses[status['context']]
+    end
+  end
+
+  def top_evergreen_status
+    status = @pull.top_evergreen_status
+    if status
+      status = EvergreenStatusPresenter.new(status, @pull, eg_client)
+    end
+    status
+  end
+
+  def evergreen_version
+    @evergreen_version ||= Evergreen::Version.new(eg_client, @pull.evergreen_version_id)
   end
 end
