96eed4319ec74a269643bb4405a2dfba26add6b1
Oleg Pudeyev <oleg@bsdpower.com>
2018-08-13 14:51:51 -0400

    Track recent distros

--- lib/fe/app.rb
+++ lib/fe/app.rb
@@ -173,6 +173,7 @@ class App < Sinatra::Base
     @keys = eg_client.keys
     @hosts = eg_client.user_hosts
     @config = SpawnConfig.first || SpawnConfig.new
+    @recent_distros = SpawnedHost.recent_distros
     slim :spawn
   end
 
@@ -183,6 +184,10 @@ class App < Sinatra::Base
     spawn_config.last_distro_name = params[:distro]
     spawn_config.last_key_name = params[:key]
     spawn_config.save!
+    SpawnedHost.create!(
+      distro_name: params[:distro],
+      key_name: params[:key],
+    )
     redirect "/spawn"
   end
 
--- lib/fe/models.rb
+++ lib/fe/models.rb
@@ -22,3 +22,17 @@ class SpawnConfig
   field :last_distro_name, type: String
   field :last_key_name, type: String
 end
+
+class SpawnedHost
+  include Mongoid::Document
+  include Mongoid::Timestamps
+
+  field :distro_name, type: String
+  field :key_name, type: String
+
+  class << self
+    def recent_distros
+      order(created_at: -1).limit(10).pluck(:distro_name).uniq[0...5]
+    end
+  end
+end
