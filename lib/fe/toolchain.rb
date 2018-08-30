c76b0f10bc632f2bb5ca3829b40489ddffc3181f
Oleg Pudeyev <oleg@bsdpower.com>
2018-08-30 15:22:06 -0400

    Fix toolchain view

--- lib/fe/toolchain.rb
+++ lib/fe/toolchain.rb
@@ -3,6 +3,6 @@ require_relative './child_process_helper'
 
 class Toolchain
   def latest_sha
-    Repo.new('10gen', 'mongo-ruby-toolchain').master_sha
+    RepoCache.new('10gen', 'mongo-ruby-toolchain').master_sha
   end
 end
