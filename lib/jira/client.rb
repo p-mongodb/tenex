05fe68a9bfad769d951d98660142a415b7b94d14
Oleg Pudeyev <oleg@bsdpower.com>
2019-01-31 20:38:09 -0500

    jira link

---
+++ lib/jira/client.rb
@@ -0,0 +1,60 @@
+autoload :JSON, 'json'
+require 'faraday'
+require 'faraday/detailed_logger'
+
+module Jira
+  class Client
+
+    class ApiError < StandardError
+      def initialize(message, status: nil)
+        super(message)
+        @status = status
+      end
+
+      attr_reader :status
+    end
+
+    def initialize(username:, password:, site:)
+      @connection ||= Faraday.new("#{site}/rest/api/latest") do |f|
+        f.request :url_encoded
+        f.response :detailed_logger
+        f.adapter Faraday.default_adapter
+        f.headers['user-agent'] = 'EvergreenRubyClient'
+        f.basic_auth(username, password)
+      end
+    end
+
+    attr_reader :connection
+
+    def get_json(url)
+      request_json(:get, url)
+    end
+
+    def post_json(url, params=nil)
+      request_json(:post, url, params)
+    end
+
+    def request_json(meth, url, params=nil)
+      response = connection.send(meth) do |req|
+        req.url(url)
+        if params
+          req.body = JSON.dump(params)
+          req.headers['content-type'] = 'application/json'
+        end
+      end
+      if response.status != 200 && response.status != 201
+        error = nil
+        begin
+          error = JSON.parse(response.body)['error']
+        rescue
+        end
+        msg = "Jira #{meth.to_s.upcase} #{url} failed: #{response.status}"
+        if error
+          msg += ": #{error}"
+        end
+        raise ApiError.new(msg, status: response.status)
+      end
+      JSON.parse(response.body)
+    end
+  end
+end
