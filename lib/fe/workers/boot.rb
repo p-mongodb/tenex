46df69aafdc285545ac85aa70f1ed3deac2c8377
Oleg Pudeyev <oleg@bsdpower.com>
2019-04-04 14:13:46 -0400

    move schedule under config

--- lib/fe/workers/boot.rb
+++ lib/fe/workers/boot.rb
@@ -3,6 +3,6 @@ require 'sidekiq-cron'
 
 require_relative './eg_bumper'
 
-schedule_file = File.join(File.dirname(__FILE__), "schedule.yml")
+schedule_file = File.join(File.dirname(__FILE__), "../../../config/schedule.yml")
 
 Sidekiq::Cron::Job.load_from_hash(YAML.load(File.read(schedule_file)))
