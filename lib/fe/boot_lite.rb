dc378befbbd8243f53866e36599c3b8036ee6e6a
Oleg Pudeyev <oleg@bsdpower.com>
2019-04-29 17:27:52 -0400

    password rotation

--- lib/fe/boot_lite.rb
+++ lib/fe/boot_lite.rb
@@ -3,5 +3,16 @@ require 'byebug'
 
 Dotenv.load(File.join(File.dirname(__FILE__), '../../.env'))
 
+if ENV['JIRA_PASSWORD_PREFIX']
+  suffix = Time.now.month
+  if suffix > 9
+    suffix = 'a'.ord + suffix - 10
+  else
+    suffix = '0'.ord + suffix
+  end
+  suffix = suffix.chr
+  ENV['JIRA_PASSWORD'] = ENV['JIRA_PASSWORD_PREFIX'] + suffix
+end
+
 autoload :Github, 'github'
 autoload :Evergreen, 'evergreen'
