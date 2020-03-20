cbfc683023abc257ecf145af023565ec071308ac
Oleg Pudeyev <oleg@bsdpower.com>
2020-03-19 22:48:11 -0400

    add evg

--- lib/fe/jira_query_builder.rb
+++ lib/fe/jira_query_builder.rb
@@ -8,7 +8,7 @@ class JiraQueryBuilder
   ).freeze
 
   PROJECTS = %w(
-    ruby mongoid server help docs docsp drivers spec website
+    ruby mongoid server help docs docsp drivers spec website evg
   ).freeze
 
   COMPONENTS = {
