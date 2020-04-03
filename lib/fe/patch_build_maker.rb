948f53cb18ff32fed14eb32c3a931f8553c30da2
Oleg Pudeyev <oleg@bsdpower.com>
2020-04-02 20:17:34 -0400

    handle utf-8 diffs

--- lib/fe/patch_build_maker.rb
+++ lib/fe/patch_build_maker.rb
@@ -3,11 +3,12 @@ require 'fe/env'
 
 autoload :ProjectDetector, 'fe/project_detector'
 autoload :RepoCache, 'fe/repo_cache'
+autoload :Byebug, 'byebug'
 
 class PatchBuildMaker
   include Env::Access
 
-  def run(eg_project_id: nil)
+  def run(eg_project_id: nil, force: false)
     config = ProjectDetector.new.project_config
     eg_project_id ||= config.eg_project_name
 
@@ -16,6 +17,17 @@ class PatchBuildMaker
 
     base_sha = rc.master_sha
     process, diff_text = ChildProcessHelper.get_output(%w(git diff upstream/master))
+    diff_text.force_encoding('utf-8')
+
+=begin
+    if force
+      begin
+        diff_text.encode('utf-16')
+      rescue
+        raise e.class
+      end
+    end
+=end
 
     patch = eg_client.create_patch(
       project_id: eg_project_id,
