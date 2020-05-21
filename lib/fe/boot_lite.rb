28d0be92999bb92f4b437c65cc12959519eff9d9
Oleg Pudeyev <oleg@bsdpower.com>
2020-05-21 14:16:04 -0400

    use oj in compat mode for correct symbol key serialization

--- lib/fe/boot_lite.rb
+++ lib/fe/boot_lite.rb
@@ -3,5 +3,8 @@ require 'byebug'
 
 Dotenv.load(File.join(File.dirname(__FILE__), '../../.env'))
 
+require 'oj'
+Oj.default_options = {mode: :compat}
+
 autoload :Github, 'github'
 autoload :Evergreen, 'evergreen'
