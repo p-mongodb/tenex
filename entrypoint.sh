39f6c3c8f0a37079734cf803dc7ddbd3ff1def33
Oleg Pudeyev <oleg@bsdpower.com>
2021-03-30 14:08:54 -0400

    fix uri

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,7 +5,7 @@ set -e
 if test -z "$MONGODB_URI"; then
   mkdir -p /data
   mongod --dbpath /data --fork --logpath /data/mongod.log
-  export MONGODB_URI='mongodb://localhost/tenex?serverSelectionTimeout=5'
+  export MONGODB_URI='mongodb://localhost/tenex?serverSelectionTimeoutMS=5000'
 fi
 
 if test -n "$DOTENV"; then
