fbb00b1fc9757dbac8693c23e9398723bb2e0dd0
Oleg Pudeyev <oleg@bsdpower.com>
2021-04-01 13:46:01 -0400

    fix buildlogger log retrieval

--- lib/evergreen/buildlogger_client.rb
+++ lib/evergreen/buildlogger_client.rb
@@ -10,8 +10,14 @@ module Evergreen
         #f.response :detailed_logger
         f.adapter Faraday.default_adapter
         f.headers['user-agent'] = 'EvergreenRubyClient'
+        # These work:
         f.headers['evergreen-api-user'] = username
         f.headers['evergreen-api-key'] = api_key
+        # These are documented in
+        # https://github.com/evergreen-ci/cedar/wiki/Rest-V1-Usage
+        # but do not work:
+        #f.headers['api-user'] = username
+        #f.headers['api-key'] = api_key
       end
       @cache_root = cache_root
     end
@@ -23,7 +29,11 @@ module Evergreen
       #connection.get("task_id/#{task_id}?proc_name=task_log&print_time=true&print_priority=true").body
       resp = nil
       10.times do
-        resp = connection.get("task_id/#{task_id}?proc_name=task_log")
+        # tags indicate which logs are desired: agent_log,task_log,system_log
+        resp = connection.get("task_id/#{task_id}?tags=task_log&print_priority=true&print_time=true")
+        # This returns task and agent logs together with no formatting
+        # or any other means of telling which line comes from where:
+        #resp = connection.get("task_id/#{task_id}")
         if resp.status == 200
           break
         elsif resp.status == 500 && resp.body =~ /service unavailable/i
